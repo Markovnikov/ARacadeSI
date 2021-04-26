e880b625c518c7d59e9922ed72c4f37e3c8ad709
Markovnikov <alex.j.blasingame@gmail.com>
2021-04-25 23:18:29 -0400

    settings page with workable button

--- app/src/main/java/com/example/space_invaders_menu/MainActivity.java
+++ app/src/main/java/com/example/space_invaders_menu/MainActivity.java
@@ -4,11 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button toStart; // button to start the game
+    private Button toSettings; // button to settings page
+    private Button toScores;  // button to the scores page
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // code that sets up the settings button
+        toSettings = (Button) findViewById(R.id.settings_button);
+        toSettings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openSettings();
+            }
+        });
     }
+
+    // code for the settings button function 
+    public void openSettings() {
+        Intent intent = new Intent(this, settings_page.class);
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/space_invaders_menu/settings_page.java
+++ app/src/main/java/com/example/space_invaders_menu/settings_page.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
 public class settings_page extends AppCompatActivity {
+
+    private Button toHome; // button back to the home page
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
+
+        // this code sets up the button back to the home page
+        toHome = (Button) findViewById(R.id.settings_back_button);
+        toHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openHome();
+            }
+        });
+    }
+
+    // this code is the function to go back to the home page
+    public void openHome() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
